a7501b64abe439ae2bd99b70ca37d39d4c5b3510
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-04-16 20:00:01 +0000

    Creating branch for BioC 3.1 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/MethylAid@102593 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/visualization.R
@@ -0,0 +1,43 @@
+##' launch a shiny app for visualization of the summarized 450k data
+##'
+##' Outliers are detected based on a set of default thresholds.
+##' To use a use-defined set of thresholds use the thresholds
+##' argument.
+##' @title visualize the summarized 450k data
+##' @param object summarizedData object
+##' @param thresholds default thresholds
+##' @param background optional summarizedData-object used as background in filter control plots
+##' @param ... for future use
+##' @return lauches a web browser with the shiny application and returns a
+##' data.frame with detected outliers
+##' @import shiny ggplot2 RColorBrewer
+##' @importFrom hexbin hexbin grid.hexagons
+##' @importFrom grid pushViewport popViewport
+##' @importFrom gridBase baseViewports
+##' @export
+##' @docType methods
+##' @examples
+##' library(minfiData)
+##' baseDir <- system.file("extdata", package="minfiData")
+##' targets <- read.450k.sheet(baseDir)
+##' data <- summarize(targets)
+##' \dontrun{
+##' visualize(data)
+##' }
+setGeneric("visualize",
+           function(object,
+                    thresholds  = list(MU = 10.50, OP = 11.75, BS = 12.75, HC = 13.25, DP = 0.95),
+                    background = NULL, ...)
+           standardGeneric("visualize")
+           )
+
+##' @rdname visualize
+setMethod("visualize", "summarizedData",
+          function(object,
+                   thresholds = list(MU = 10.50, OP = 11.75, BS = 12.75, HC = 13.25, DP = 0.95),
+                   background = NULL, ...)
+          {
+            app <- list(ui=ui450k(object),
+                        server=server450k(object, thresholds = thresholds, background = background, ...))
+            invisible(runApp(app))
+          })
